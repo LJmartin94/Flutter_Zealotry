c60b295c0b96d92ae37a8a870c56dba8585c5471
limartin <lindsayjamesmartin94@gmail.com>
2023-06-10 19:33:27 +0200

    Make main a 'Hello World' text view

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Text("Hello World"),
+    );
+  }
+}
