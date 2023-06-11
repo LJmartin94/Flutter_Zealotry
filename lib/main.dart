cdcad52d11a0b8328492caea0aa3a9ae9a4774c0
limartin <lindsayjamesmartin94@gmail.com>
2023-06-11 19:40:29 +0200

    Add list of 'morning routine' buttons, all of which do nothing at the moment.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.light(primary: Color(0xFF808080)),
+        colorScheme: const ColorScheme.light(primary: Color(0xFF808080)),
       ),
       home: RandomWords(),
     );
@@ -22,7 +22,46 @@ class RandomWords extends StatefulWidget {
   RandomWordsState createState() => RandomWordsState();
 }
 
+final List<String> morningButtons = <String>[
+  "Wake Up",
+  "Get Up",
+  "Shower",
+  "News",
+  "Open Curtains",
+  "Coffee",
+  "Smoothie",
+  "Water",
+  "Vitamins",
+  "Dishes",
+  "Breakfast",
+  "Water Plants",
+  "Meditation",
+  "Language",
+  "Brush Teeth",
+  "Cosmeticism",
+  "Pack Bag",
+  "Make Bed",
+  "Take out the Bins",
+];
+
 class RandomWordsState extends State<RandomWords> {
+  final Widget _buildList = ListView.builder(
+    itemCount: morningButtons.length,
+    padding: const EdgeInsets.all(2),
+    itemBuilder: (BuildContext context, int index) {
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        color: Colors.grey[400],
+        child: Center(
+          child: TextButton(
+            onPressed: null,
+            child: Text(morningButtons[index]),
+          ),
+        ),
+      );
+    },
+  );
+
   final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
@@ -30,9 +69,7 @@ class RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: const Text("Zealotry Flutter"),
       ),
-      body: Center(
-        child: Text("${wordPair.first} ${wordPair.second}"),
-      ),
+      body: _buildList,
     );
   }
 }
