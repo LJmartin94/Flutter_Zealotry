61a38392e560a807ff53025cdb303b7fc1ecb9e2
limartin <lindsayjamesmartin94@gmail.com>
2023-06-12 10:57:21 +0200

    Change _buildList from a variable to a method, which allows other class member variables and methods to be placed inside the RandomWordsState class.

--- lib/main.dart
+++ lib/main.dart
@@ -22,66 +22,67 @@ class RandomWords extends StatefulWidget {
   RandomWordsState createState() => RandomWordsState();
 }
 
-final List<String> morningButtons = <String>[
-  "Wake Up",
-  "Get Up",
-  "Shower",
-  "News",
-  "Open Curtains",
-  "Coffee",
-  "Smoothie",
-  "Water",
-  "Vitamins",
-  "Dishes",
-  "Breakfast",
-  "Water Plants",
-  "Meditation",
-  "Language",
-  "Brush Teeth",
-  "Cosmeticism",
-  "Pack Bag",
-  "Make Bed",
-  "Take out the Bins",
-];
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final List<String> _morningButtons = <String>[
+    "Wake Up",
+    "Get Up",
+    "Shower",
+    "News",
+    "Open Curtains",
+    "Coffee",
+    "Smoothie",
+    "Water",
+    "Vitamins",
+    "Dishes",
+    "Breakfast",
+    "Water Plants",
+    "Meditation",
+    "Language",
+    "Brush Teeth",
+    "Cosmeticism",
+    "Pack Bag",
+    "Make Bed",
+    "Take out the Bins",
+  ];
 
-final _randomWordPairs = <WordPair>[];
+  Widget _buildList() {
+    return ListView.builder(
+      // itemCount: morningButtons.length,
+      padding: const EdgeInsets.all(2),
+      itemBuilder: (BuildContext context, int index) {
+        if (index >= _morningButtons.length + _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        if (index < _morningButtons.length) {
+          return _buildRow(_morningButtons[index]);
+        } else {
+          return _buildRow(
+              _randomWordPairs[index - _morningButtons.length].asPascalCase);
+        }
+      },
+    );
+  }
 
-Widget _buildRow(content) {
-  return Container(
-    margin: const EdgeInsets.symmetric(vertical: 4),
-    child: Ink(
-      color: Colors.grey[300],
-      child: InkWell(
-        splashColor: Colors.grey,
-        splashFactory: InkRipple.splashFactory,
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Text(content),
+  Widget _buildRow(content) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: Ink(
+        color: Colors.grey[300],
+        child: InkWell(
+          splashColor: Colors.grey,
+          splashFactory: InkRipple.splashFactory,
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: Text(content),
+            ),
           ),
         ),
       ),
-    ),
-  );
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final Widget _buildList = ListView.builder(
-    // itemCount: morningButtons.length,
-    padding: const EdgeInsets.all(2),
-    itemBuilder: (BuildContext context, int index) {
-      if (index >= morningButtons.length + _randomWordPairs.length) {
-        _randomWordPairs.addAll(generateWordPairs().take(10));
-      }
-      if (index < morningButtons.length) {
-        return _buildRow(morningButtons[index]);
-      } else {
-        return _buildRow(
-            _randomWordPairs[index - morningButtons.length].asPascalCase);
-      }
-    },
-  );
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +90,7 @@ class RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: const Text("Zealotry Flutter"),
       ),
-      body: _buildList,
+      body: _buildList(),
     );
   }
 }
