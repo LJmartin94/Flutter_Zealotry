6bba15bf417cfc3b4a65b79ce27ecb71b677e109
limartin <lindsayjamesmartin94@gmail.com>
2023-06-12 09:12:33 +0200

    Move container display logic of list view builder to separate function. Add random words below morning menu for testing.

--- lib/main.dart
+++ lib/main.dart
@@ -44,32 +44,45 @@ final List<String> morningButtons = <String>[
   "Take out the Bins",
 ];
 
-class RandomWordsState extends State<RandomWords> {
-  final Widget _buildList = ListView.builder(
-    itemCount: morningButtons.length,
-    padding: const EdgeInsets.all(2),
-    itemBuilder: (BuildContext context, int index) {
-      return Container(
-        margin: const EdgeInsets.symmetric(vertical: 4),
-        child: Ink(
-          color: Colors.grey[300],
-          child: InkWell(
-            splashColor: Colors.grey,
-            splashFactory: InkRipple.splashFactory,
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Center(
-                child: Text(morningButtons[index]),
-              ),
-            ),
+final _randomWordPairs = <WordPair>[];
+
+Widget _buildRow(content) {
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 4),
+    child: Ink(
+      color: Colors.grey[300],
+      child: InkWell(
+        splashColor: Colors.grey,
+        splashFactory: InkRipple.splashFactory,
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Text(content),
           ),
         ),
-      );
+      ),
+    ),
+  );
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final Widget _buildList = ListView.builder(
+    // itemCount: morningButtons.length,
+    padding: const EdgeInsets.all(2),
+    itemBuilder: (BuildContext context, int index) {
+      if (index >= morningButtons.length + _randomWordPairs.length) {
+        _randomWordPairs.addAll(generateWordPairs().take(10));
+      }
+      if (index < morningButtons.length) {
+        return _buildRow(morningButtons[index]);
+      } else {
+        return _buildRow(
+            _randomWordPairs[index - morningButtons.length].asPascalCase);
+      }
     },
   );
 
-  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
